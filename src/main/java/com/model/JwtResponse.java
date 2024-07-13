a99cb9f5c881d8cd4a980dd875b3d89237bb579b
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-13 08:31:18 +0530

    Jwt Response

---
+++ src/main/java/com/model/JwtResponse.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+    private String jwtToken;
+    private String login_id;
+
+}
