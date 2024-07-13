841282e438667619ba5ff00e8f384f98573a68f3
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-13 08:30:24 +0530

    Customer

---
+++ src/main/java/com/model/Customer.java
@@ -0,0 +1,33 @@
+package com.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers_info")
+public class Customer {
+    @Id
+    @Column(name = "id")
+    private String uuid;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String street;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String email;
+
+    private Long phone;
+}
