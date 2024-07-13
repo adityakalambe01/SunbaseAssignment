ede8d799b09ffd4b6fa87dff081f38a9788bee6a
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-13 08:32:23 +0530

    Main

---
+++ src/main/java/com/SunbaseCustomersApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunbaseCustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunbaseCustomersApplication.class, args);
+    }
+
+}
