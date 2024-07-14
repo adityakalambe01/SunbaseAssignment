af93b207809875989da501c2985b88bd28560a0a
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-15 03:48:30 +0530

    Customer Controller Update

--- src/main/java/com/controller/CustomerController.java
+++ src/main/java/com/controller/CustomerController.java
@@ -11,12 +11,12 @@ import java.util.Random;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "/home")
-public class HomeController {
+@RequestMapping(value = "/api")
+public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping
+    @GetMapping(value = "/customer-list")
     public List<Customer> getAllCustomers(
             @RequestParam(value = "page",defaultValue = "0", required = false) Integer pageNumber,
             @RequestParam(value = "size", defaultValue = "8", required = false) Integer pageSize
@@ -35,19 +35,27 @@ public class HomeController {
     }
 
     //Add Customer
-    @PostMapping
+    @PostMapping(value = "/create-customer")
     public boolean addNewCustomer(Customer customer) {
         return customerService.addNewCustomer(customer)!=null;
     }
 
-    @PutMapping
-    public boolean updateCustomer(String customerId, Customer customer) {
+    @PutMapping(value = "/update-customer/{id}")
+    public boolean updateCustomer(@PathVariable("id") String customerId, Customer customer) {
         return customerService.udateExistingCustomer(customerId, customer)!=null;
     }
 
     @DeleteMapping("{id}")
     public boolean deleteCustomer(@PathVariable("id") String customerId) {
         return customerService.deleteCustomer(customerId);
+    }
+
+    @PostMapping("/customer-sync")
+    public void syncCustomers() {
+        final String apiUrl = "https://qa.sunbasedata.com/sunbase/portal/api/assignment.jsp?cmd=get_customer_list";
+        String bearerToken = "dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM=";
+
+        customerService.callExternalApi(apiUrl, bearerToken);
     }
 
     @PostMapping("addDefault")
@@ -64,26 +72,19 @@ public class HomeController {
         if (customerService.getCountOfCustomers()==0){
             int length = firstNameArray.length;
             for (int i = 0; i < 20; i++) {
-                Customer customer = new Customer();
-
-                customer.setUuid(UUID.randomUUID().toString());
-
-                customer.setFirst_name(firstNameArray[new Random().nextInt(length)]);
-
-                customer.setLast_name(lastNameArray[new Random().nextInt(length)]);
-
-                customer.setStreet(street[new Random().nextInt(length)]);
-
-                customer.setAddress(address[new Random().nextInt(length)]);
-
-                customer.setCity(city[new Random().nextInt(length)]);
-
-                customer.setState(state[new Random().nextInt(length)]);
-
-                customer.setEmail(
-                        customer.getFirst_name().toLowerCase()+customer.getLast_name().toLowerCase()+new Random().nextInt(10000)+"@gmail.com"
+                StringBuffer firstName = new StringBuffer(firstNameArray[new Random().nextInt(length)]);
+                StringBuffer lastName = new StringBuffer(lastNameArray[new Random().nextInt(length)]);
+                Customer customer = new Customer(
+                        UUID.randomUUID().toString(),
+                        firstName.toString(),
+                        lastName.toString(),
+                        street[new Random().nextInt(length)],
+                        address[new Random().nextInt(length)],
+                        city[new Random().nextInt(length)],
+                        state[new Random().nextInt(length)],
+                        firstName.toString()+lastName.toString()+new Random().nextInt(10000)+"@gmail.com",
+                        String.valueOf(new Random().nextLong(15000))
                 );
-                customer.setPhone(new Random().nextLong(15000));
                 customerService.addNewCustomer(customer);
             }
         }
