4edb6a95f007ba525f8272fdac3026a16c70eb92
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-15 03:47:17 +0530

    Service Update

---
+++ src/main/java/com/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.service;
+
+import com.model.User;
+import com.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailId(username).orElseThrow(()->new RuntimeException("User not found"));
+        return user;
+    }
+}
--- src/main/java/com/service/CustomerService.java
+++ src/main/java/com/service/CustomerService.java
@@ -1,12 +1,19 @@
 package com.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.model.Customer;
 import com.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CustomerService {
@@ -15,6 +22,7 @@ public class CustomerService {
 
     //Add Customer
     public Customer addNewCustomer(Customer customer) {
+        customer.setUuid(UUID.randomUUID().toString());
         return customerRepository.save(customer);
     }
 
@@ -77,5 +85,37 @@ public class CustomerService {
 
     public Long getCountOfCustomers() {
         return customerRepository.count();
+    }
+
+    public void callExternalApi(String apiUrl, String bearerToken){
+        StringBuilder response = new StringBuilder();
+        List<Customer> customerList = null;
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Authorization", "Bearer " + bearerToken);
+            conn.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+            conn.disconnect();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            customerList = objectMapper.readValue(response.toString(), new TypeReference<List<Customer>>() {});
+        }catch (Exception e){
+
+        }
+
+        /*for (Customer customer : customerList) {
+            System.out.println(customer);
+        }*/
+
+        customerRepository.saveAll(customerList);
+
     }
 }
