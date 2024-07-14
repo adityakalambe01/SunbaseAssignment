e7c1090dd65ab83404678edae226e202b1a59e33
Aditya Kalambe <adityakalambe20@gmail.com>
2024-07-15 03:48:50 +0530

    Redirect Controller Update

--- src/main/java/com/controller/Redirect.java
+++ src/main/java/com/controller/Redirect.java
@@ -1,17 +1,44 @@
 package com.controller;
 
+import com.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@Controller
 @RequestMapping(value = "/user")
 public class Redirect {
-    @RequestMapping(value = "/login")
+    @Autowired
+    private CustomerController customerController;
+
+
+    @GetMapping(value = "/login")
     public String homePage(Model model){
-        return "login.html";
+        return "login";
+    }
+
+    @GetMapping(value = "/customers")
+    public String customersPage(
+            Model model,
+            @RequestParam(value = "page",defaultValue = "0", required = false) Integer pageNumber,
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer pageSize
+    ){
+        model.addAttribute("allCustomers", customerController.getAllCustomers(pageNumber,pageSize));
+        return "customerList";
+    }
+
+    @GetMapping(value = "/add-customer")
+    public String addCustomer(Model model){
+        return "addCustomer";
+    }
+
+    @GetMapping(value = "/sync")
+    public String sync(Model model){
+        customerController.syncCustomers();
+        return "Done";
     }
 }
